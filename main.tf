82e95556c89ea7ed8cc7d73c92846b91c8019fc5
Bullaiah <bullaiahd@gmail.com>
2025-04-16 19:04:43 +0530

    terraform github actions

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "ec2" {
+    ami = "ami-00a929b66ed6e0de6"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "Terraform-Jenkins"
+    }
+}
