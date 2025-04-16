ee0e25673050a6d2aef6fb5ed371626ef1cc9358
Bullaiah <bullaiahd@gmail.com>
2025-04-16 19:24:29 +0530

    changed to github actions

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "ec2" {
     ami = "ami-00a929b66ed6e0de6"
     instance_type = "t2.micro"
     tags = {
-        Name = "Terraform-Jenkins"
+        Name = "Terraform-github-actions"
     }
 }
